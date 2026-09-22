dd46d91d98ffa1eeb31ec6d578ffa5f2aaa60cab
ajaxlau <428955+ajaxlau@users.noreply.github.com>
2026-09-22 10:05:58 +0800

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-console.log("3DViewerPlus PWA starting...");
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
